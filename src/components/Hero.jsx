02d1d111b2c4f92af9b79d5b5d2b21c33b754cee
Ehteshamul Haque <ehteshamulhaque5103@gmail.com>
2023-09-09 13:51:46 +0530

    Customised the site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,16 @@ const Hero = () => {
   return (
       <header className="w-full flex justify-center items-center flex-col">
           <nav className="flex justify-between items-center w-full mb-10 pt-3">
-              <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
+              <img src={logo} alt="sumz-logo" className="w-60 object-contain" />
               <button type="button" onClick={()=>window.open('https://github.com/ehteshamul51haque')} className='black_btn'>GitHub</button>
           </nav>
           <h1 className='head_text'>
               Summarize Articles with <br className='max-md:hidden'/>
-              <span className='orange_gradient'>OpenAI GPT-4</span>
+              <span className='purple_gradient'>SummaWise</span>
           </h1>
           <h2 className='desc'>
-              Simplify your reading with Summize, an open-source article summarizer
-              that transforms lengthy articles into clear and concise summaries
+              Elevate your reading with SummaWise,
+              an open-source article summarizer that simplifies lengthy articles into concise, time-saving summaries.
           </h2>
     </header>
   )
